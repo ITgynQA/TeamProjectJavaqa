6ab88918f1aea427718dc93d8a9eb69ba625b630
Ekaterina Evdokimova <kate_evd79@mail.ru>
2023-04-22 12:27:04 +0400

    New tests added

--- src/test/java/ru/netology/teamproject/CreditAccountTest.java
+++ src/test/java/ru/netology/teamproject/CreditAccountTest.java
@@ -30,12 +30,12 @@ public class CreditAccountTest {
     @Test
     public void shouldCreateAccountIfInitialBalanceNegative() {
 
-         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-              new CreditAccount(
-                     -1,
-                     5_000,
-                     15
-             );
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new CreditAccount(
+                    -1,
+                    5_000,
+                    15
+            );
         });
     }
 
@@ -63,7 +63,7 @@ public class CreditAccountTest {
     public void shouldCreateAccountIfCreditLimitNegative() {
 
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-             new CreditAccount(
+            new CreditAccount(
                     1000,
                     -1,
                     15
@@ -94,7 +94,7 @@ public class CreditAccountTest {
     @Test
     public void shouldCreateAccountIfRateNegative() {
 
-         Assertions.assertThrows(IllegalArgumentException.class, () -> {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
             new CreditAccount(
                     0,
                     5000,
@@ -137,6 +137,16 @@ public class CreditAccountTest {
     }
 
     @Test
+    public void shouldPositivePayBelowCreditLimitAssertTrue() {
+        CreditAccount account = new CreditAccount(
+                5000,
+                2000,
+                10
+        );
+        Assertions.assertTrue(account.pay(6000));
+    }
+
+    @Test
     public void shouldPositivePayAboveLimitIfInitialBalancePositive() {
         CreditAccount account = new CreditAccount(
                 1000,
@@ -146,6 +156,40 @@ public class CreditAccountTest {
         account.pay(4000);
 
         Assertions.assertEquals(1000, account.getBalance());
+    }
+
+    @Test
+    public void shouldPositivePayAboveLimitAssertFalse() {
+        CreditAccount account = new CreditAccount(
+                1000,
+                2000,
+                10
+        );
+
+        Assertions.assertFalse(account.pay(4000));
+    }
+
+    @Test
+    public void shouldPositivePayEqualsLimitIfInitialBalancePositive() {
+        CreditAccount account = new CreditAccount(
+                2000,
+                3000,
+                10
+        );
+        account.pay(5000);
+
+        Assertions.assertEquals(-3000, account.getBalance());
+    }
+
+    @Test
+    public void shouldPositivePayEqualsLimitAssertTrue() {
+        CreditAccount account = new CreditAccount(
+                2000,
+                3000,
+                10
+        );
+
+        Assertions.assertTrue(account.pay(5000));
     }
 
     @Test
@@ -209,6 +253,16 @@ public class CreditAccountTest {
     }
 
     @Test
+    public void shouldAddNegativeAmountAssertFalse() {
+        CreditAccount account = new CreditAccount(
+                5000,
+                2000,
+                10
+        );
+        Assertions.assertFalse(account.add(-50));
+    }
+
+    @Test
     public void shouldAddNegativeAmountIfInitialBalanceZero() {
         CreditAccount account = new CreditAccount(
                 0,
@@ -257,7 +311,18 @@ public class CreditAccountTest {
     }
 
     @Test
-    public void shouldYearChangeToPositiveBalance() {
+    public void shouldAddPositiveAmountAssertTrue() {
+        CreditAccount account = new CreditAccount(
+                5000,
+                2000,
+                10
+        );
+
+        Assertions.assertTrue(account.add(1000));
+    }
+
+    @Test
+    public void shouldCalculatePercentToPositiveBalance() {
         CreditAccount account = new CreditAccount(
                 200,
                 5_000,
@@ -268,13 +333,111 @@ public class CreditAccountTest {
     }
 
     @Test
-    public void shouldYearChangeToNegativeBalance() {
+    public void shouldCalculatePercentToBalanceZero() {
         CreditAccount account = new CreditAccount(
-                -200,
+                0,
                 5_000,
                 15
         );
 
+        Assertions.assertEquals(0, account.yearChange());
+    }
+
+    @Test
+    public void shouldCalculatePercentToNegativeBalance() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+        account.pay(200);
+
         Assertions.assertEquals(-30, account.yearChange());
     }
+
+    @Test
+    public void shouldCalculatePercentToNegativeBalanceOddNumber() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+        account.pay(555);
+
+        Assertions.assertEquals(-83, account.yearChange());
+    }
+
+    @Test
+    public void shouldSetToPositiveRate() {
+        CreditAccount account = new CreditAccount(
+                2000,
+                5000,
+                15
+        );
+        account.setRate(10);
+
+        Assertions.assertEquals(10, account.getRate());
+    }
+
+    @Test
+    public void shouldSetToZeroRate() {
+        CreditAccount account = new CreditAccount(
+                2000,
+                5000,
+                15
+        );
+        account.setRate(0);
+
+        Assertions.assertEquals(0, account.getRate());
+    }
+
+    @Test
+    public void shouldSetToNegativeRate() {
+        CreditAccount account = new CreditAccount(
+                2000,
+                5000,
+                15
+        );
+        account.setRate(-10);
+
+        Assertions.assertEquals(15, account.getRate());
+    }
+
+    @Test
+    public void shouldIncreaseCreditLimit() {
+        CreditAccount account = new CreditAccount(
+                2000,
+                5000,
+                15
+        );
+        account.setCreditLimit(6000);
+
+        Assertions.assertEquals(6000, account.getCreditLimit());
+    }
+
+    @Test
+    public void shouldDecreaseCreditLimit() {
+        CreditAccount account = new CreditAccount(
+                5000,
+                15,
+                15
+        );
+        account.setCreditLimit(4000);
+
+        Assertions.assertEquals(4000, account.getCreditLimit());
+    }
+
+    @Test
+    public void shouldSetCreditLimitToZero() {
+        CreditAccount account = new CreditAccount(
+                2000,
+                5000,
+                15
+        );
+        account.setCreditLimit(0);
+
+        Assertions.assertEquals(0, account.getCreditLimit());
+    }
+
+
 }
